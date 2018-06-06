f5a93fd9475a71f59999dec3b987392c54421eb7
Daniel <danielulisesverdin@gmail.com>
2018-06-06 16:48:18 -0500

    Writing our own createStore function from scratch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 
-import { createStore } from 'redux';
+import counter from './indexReducer';
 
-import './index.scss';
+const createStore = (reducer) => {
+	let state;
+	let listeners = [];
 
-const counter = (state = 0, action) => {
-	switch (action.type) {
-	case 'INCREMENT':
-		return state + 1;
-	case 'DECREMENT':
-		return state - 1;
-	default:
-		return state;
-	}
+	const getState = () => state;
+
+	const  dispatch = (action) => {
+		state = reducer(state, action);
+		listeners.forEach(listener => {
+			listener();
+		});
+	};
+	const subscribe = (listener) => {
+		listeners.push(listener);
+		return () => {
+			listeners = listeners.filter((l) => l !== listener);
+		};
+	};
+
+	dispatch({});
+
+	return { getState, dispatch, subscribe };
 };
 
 const store = createStore(counter);
