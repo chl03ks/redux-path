dfba9fb09cb7f92b3fe196397b31a1ef9ccab724
Daniel <danielulisesverdin@gmail.com>
2018-06-06 16:59:05 -0500

    create-react-app boilerplate to use redux on it

--- src/index.js
+++ src/index.js
@@ -1,44 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import counter from './indexReducer';
-
-const createStore = (reducer) => {
-	let state;
-	let listeners = [];
-
-	const getState = () => state;
-
-	const  dispatch = (action) => {
-		state = reducer(state, action);
-		listeners.forEach(listener => {
-			listener();
-		});
-	};
-	const subscribe = (listener) => {
-		listeners.push(listener);
-		return () => {
-			listeners = listeners.filter((l) => l !== listener);
-		};
-	};
-
-	dispatch({});
-
-	return { getState, dispatch, subscribe };
-};
-
-const store = createStore(counter);
-
-const render = () => {
-	document.body.innerText = store.getState();
-};
-
-store.subscribe(render);
-render();
-
-
-document.addEventListener('click', () => {
-	store.dispatch({ type: 'INCREMENT'});
-});
-
-
-export default counter;
-
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
