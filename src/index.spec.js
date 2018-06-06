88eaf704d2febba02993552ac317ccc94e8fcf25
Daniel <danielulisesverdin@gmail.com>
2018-06-06 16:48:36 -0500

    Importing the counter function from the Reducer file

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,19 +1,19 @@
-import counter from './index';
+import counter from './indexReducer';
 
 test('Should icrement and ', () => {
-  expect(
-    counter(0, { type: 'INCREMENT'})
-  ).toEqual(1);
+	expect(
+		counter(0, { type: 'INCREMENT'})
+	).toEqual(1);
 
-  expect(
-    counter(1, { type: 'INCREMENT'})
-  ).toEqual(2);
+	expect(
+		counter(1, { type: 'INCREMENT'})
+	).toEqual(2);
 
-  expect(
-    counter(2, { type: 'DECREMENT'})
-  ).toEqual(1);
+	expect(
+		counter(2, { type: 'DECREMENT'})
+	).toEqual(1);
 
-  expect(
-    counter(1, { type: 'DECREMENT'})
-  ).toEqual(0);
+	expect(
+		counter(1, { type: 'DECREMENT'})
+	).toEqual(0);
 });
