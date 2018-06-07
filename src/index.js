b832ac08500039d4bdef9d90449e82a6b859fcc8
Daniel <danielulisesverdin@gmail.com>
2018-06-07 12:10:09 -0500

    Simple application using react and redux together

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import Counter from './componets/Counter';
+import CounterReducer from './componets/CounterReducer';
+
+const store = createStore(CounterReducer);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter 
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT'})}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT'})}
+    />, 
+  document.getElementById('root'));
+}
+
+store.subscribe(render);
+
+render();
+
 registerServiceWorker();
