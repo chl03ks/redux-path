a98a1b207ce81a220d6aaf6570fa1b30ea69ee44
Daniel <danielulisesverdin@gmail.com>
2018-06-07 17:30:33 -0500

    Reducer composition, how we can use this to update items in an array

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,24 +1,33 @@
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOOGLE_TODO':
+      if(state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        completed: !state.completed
+      };
+    default:
+      return state;
+  }
+}
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+       todo(undefined, action)
       ];
     case 'TOOGLE_TODO':
-      return state.map((todo) => {
-        if(todo.id !== action.id) {
-          return todo;
-        }
-        return {
-          ...todo,
-          completed: !todo.completed
-        };
-      });
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
