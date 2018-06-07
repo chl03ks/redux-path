3ba050934a60b9a3499a671238d41bb16ae750fe
Daniel <danielulisesverdin@gmail.com>
2018-06-07 18:22:48 -0500

    Todo list application with the old reducers

--- src/index.js
+++ src/index.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore }  from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, combineReducers } from 'redux';
-import Counter from './componets/Counter';
-import CounterReducer from './componets/CounterReducer';
+import './index.css';
 
-const appReducer = combineReducers({
-  CounterReducer,
-});
+import todos from './reducers/todos';
 
-const store = createStore(appReducer);
+const store = createStore(todos);
+
+let nextId = 0;
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <input ref={node => {this.input = node}} />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextId++
+          });
+          this.input.value = '';
+        }}> Add Todo </button>
+        <ul>
+          {this.props.todos.map((t) => 
+            (
+              <li key={t.id}
+                  onClick={() => {
+                    store.dispatch({ type: 'TOOGLE_TODO', id: t.id })}
+                  } 
+                  style={{ 
+                    textDecoration: t.completed ? 'line-through' : 'none'
+                  }}> 
+                {t.text}
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
 
 const render = () => {
   ReactDOM.render(
-    <Counter 
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT'})}
-      onDecrement={() => store.dispatch({ type: 'DECREMENT'})}
-    />, 
-  document.getElementById('root'));
+    <App todos={store.getState()} />, 
+    document.getElementById('root')
+  );
 }
 
 store.subscribe(render);
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -10,6 +10,7 @@ const todo = (state, action) => {
       if(state.id !== action.id) {
         return state;
       }
+
       return {
         ...state,
         completed: !state.completed
