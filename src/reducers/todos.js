5aa97bb2f27adde236d7445fb1ae05a45960a103
Daniel <danielulisesverdin@gmail.com>
2018-06-07 17:24:49 -0500

    Todo reducer , and the state using deep freeze

---
+++ src/reducers/todos.js
@@ -0,0 +1,27 @@
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ];
+    case 'TOOGLE_TODO':
+      return state.map((todo) => {
+        if(todo.id !== action.id) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      });
+    default:
+      return state;
+  }
+}
+
+export default todos;
