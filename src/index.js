95cbce37aa3e4f4449fe55fc7e49178318602b38
Daniel <danielulisesverdin@gmail.com>
2018-06-07 20:06:48 -0500

    Separating the presentationa components in to another folder Todo and TodoList

--- src/index.js
+++ src/index.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { createStore }  from 'redux';
+import { createStore, combineReducers }  from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
+import TodoList from './components/TodoList';
+
 import todos from './reducers/todos';
 
-const store = createStore(todos);
+import visibilityFilter from './reducers/visibilityFilter';
+
+const reducers = combineReducers({todos, visibilityFilter });
+
+const store = createStore(reducers);
 
 let nextId = 0;
 
-export default class App extends Component {
+const FilterLink = ({filter , children, currentFilter }) => {
+  if(filter === currentFilter) {
+    return <span> {children} </span>
+  }
+
+  return (
+    <a href="/" onClick={e => {
+         e.preventDefault();
+         store.dispatch({ type: 'SET_VISIBILITY_FILTER', filter })
+       }}>
+       { children }
+    </a>
+  );
+}
+
+class App extends Component {
+
+  getVisiblesTodos(todos, filter) {
+    switch (filter) {
+      case 'SHOW_ALL':
+        return todos;
+      case 'SHOW_COMPLETED':
+        return todos.filter(t => t.completed);
+      case 'SHOW_ACTIVE': 
+        return todos.filter(t => !t.completed);
+      default:
+        return todos;
+    }
+  }
+
   render() {
+    const { todos, visibilityFilter } = this.props;
+    const visibleTodos = this.getVisiblesTodos(
+      todos,
+      visibilityFilter,
+    );
+
     return (
       <div>
         <input ref={node => {this.input = node}} />
@@ -23,22 +64,34 @@ export default class App extends Component {
             id: nextId++
           });
           this.input.value = '';
-        }}> Add Todo </button>
-        <ul>
-          {this.props.todos.map((t) => 
-            (
-              <li key={t.id}
-                  onClick={() => {
-                    store.dispatch({ type: 'TOOGLE_TODO', id: t.id })}
-                  } 
-                  style={{ 
-                    textDecoration: t.completed ? 'line-through' : 'none'
-                  }}> 
-                {t.text}
-              </li>
-            )
-          )}
-        </ul>
+        }}> Add Todo  </button>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id => {
+            store.dispatch({ type: 'TOOGLE_TODO', id })
+          }}
+         />
+        <p>
+          Show: 
+          <FilterLink 
+            filter='SHOW_ALL'
+            currentFilter={visibilityFilter}>
+            All
+          </FilterLink>
+          { '  ' }
+          <FilterLink 
+            filter='SHOW_ACTIVE' 
+            currentFilter={visibilityFilter}>
+            Active
+          </FilterLink>
+          { '  ' }
+          <FilterLink 
+            filter='SHOW_COMPLETED' 
+            currentFilter={visibilityFilter}>
+            Completed
+          </FilterLink>
+          { '  ' }
+        </p>
       </div>
     )
   }
@@ -46,7 +99,7 @@ export default class App extends Component {
 
 const render = () => {
   ReactDOM.render(
-    <App todos={store.getState()} />, 
+    <App {...store.getState()} />, 
     document.getElementById('root')
   );
 }
